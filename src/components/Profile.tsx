4aefc79d50a1391230b28c7f58b4ac7c5fdfd101
thanaphon <e21bvz@gmail.com>
2024-06-14 12:57:52 +0700

    deploy netify

---
+++ src/components/Profile.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+function Profile({ user }: any) {
+  if (!user)
+    return (
+      <div>
+        <p>ไม่พบข้อมูล</p>
+        <div className="curser-pointer bg-green-400 text-black rounded-[10px] p-5 font-bold w-full  my-4 mb-2">
+          ล็อคอินด้วย Line
+        </div>
+      </div>
+    );
+
+  return (
+    <>
+      <div className="py-4">
+        <img
+          src={user.url}
+          className="rounded-full w-[120px] h-[120px] mx-auto m-4"
+        />
+      </div>
+      <h1 className="font-bold text-[1.4rem] text-center">ข้อมูลผู้ Login ปัจจุบัน</h1>
+      <hr className="my-5" />
+      <div className="mb-4">
+        <h1 className="font-bold text-[1.3rem]">ชื่อ</h1>
+        <p>{user.username}</p>
+      </div>
+      <div className="mb-4">
+        <h1 className="font-bold text-[1.3rem]">ลิงค์</h1>
+        <p>{user.url}</p>
+      </div>
+      <div className="mb-4">
+        <h1 className="font-bold text-[1.3rem]">อีเมลล์</h1>
+        <p>{user.email}</p>
+      </div>
+      <div className="mb-4">
+        <h1 className="font-bold text-[1.3rem]">LineId</h1>
+        <p>{user.lineId}</p>
+      </div>
+      <div className="mb-4">
+        <h1 className="font-bold text-[1.3rem]">UserId</h1>
+        <p>{user.userId}</p>
+      </div>
+      <div className="mb-4">
+        <h1 className="font-bold text-[1.3rem]">JsonData</h1>
+        <div className="p-4  bg-slate-300 rounded-[10px] my-3">
+          <pre className="text-[0.8rem] text-nowrap">{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      </div>
+
+      <div className="sm:flex justify-content-between mt-[2px]">
+        <div className="curser-pointer bg-green-400 text-black rounded-[10px] p-5 font-bold w-full mb-2 sm:mr-1">
+          ส่งข้อความหาฉัน
+        </div>
+        <div className="curser-pointer bg-green-400 text-black rounded-[10px] p-5 font-bold w-full  mb-2">
+          ส่งข้อความหาภายในกลุ่ม
+        </div>
+      </div>
+      <div className="mt-3">
+        <a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6 inline-block align-middle mr-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </svg>
+          Download Postman Document
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
---
+++ src/pages/index.tsx
@@ -0,0 +1,115 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Profile from "@/components/Profile";
+import ListUser from "@/components/ListUser";
+import { useEffect } from "react";
+import liff from "@line/liff";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const user = [
+    {
+      username: "admin1",
+      password: "admin",
+      url: "image/profile.jpg",
+      email: "admin1@example.com",
+      lineId: "line1",
+      userId: "user1"
+    },
+    {
+      username: "admin2",
+      password: "admin",
+      url: "image/profile.jpg",
+      email: "admin2@example.com",
+      lineId: "line2",
+      userId: "user2"
+    },
+    {
+      username: "admin3",
+      password: "admin",
+      url: "image/profile.jpg",
+      email: "admin3@example.com",
+      lineId: "line3",
+      userId: "user3"
+    },
+    {
+      username: "admin4",
+      password: "admin",
+      url: "image/profile.jpg",
+      email: "admin4@example.com",
+      lineId: "line4",
+      userId: "user4"
+    },
+    {
+      username: "admin5",
+      password: "admin",
+      url: "image/profile.jpg",
+      email: "admin5@example.com",
+      lineId: "line5",
+      userId: "user5"
+    }
+  ];
+
+  let LiffID = "2002643017-7pYpek5O";
+  let LiffUrl = "https://liff.line.me/2002643017-7pYpek5O";
+
+  const fetchUserProfile = async () => {
+    try {
+      liff.getProfile().then((profile) => {
+        console.log(profile);
+      });
+    } catch (error) {
+      console.error("Error fetching profile: ", error);
+    }
+  };
+  
+  const loginInit = () => {
+    liff
+      .init({
+        liffId: LiffID,
+        withLoginOnExternalBrowser: true
+      })
+      .then(() => {
+        liff.ready.then(async () => {
+          if (liff.isInClient() || liff.isLoggedIn()) {
+            await fetchUserProfile();
+          } else {
+            await liff.login({
+              redirectUri: LiffUrl
+            });
+          }
+        });
+      });
+  };
+
+  useEffect(() => {
+    loginInit();
+  }, []);
+  return (
+    <div className="my-5">
+      <div className="sm:text-center mb-4 p-6 bg-white sm:m-4 m-4 rounded-[10px]">
+        <p className="sm:text-[3rem] font-bold text-green-500">
+          Line-Login-Taining
+        </p>
+        <h1 className="mt-3">
+          <span className="font-bold">จำนวนทั้งหมด</span> {user.length}
+        </h1>
+        <h1>
+          <span className="font-bold">Database</span> : mongo-db
+        </h1>
+      </div>
+      <div className="sm:grid grid-cols-2 gap-3 m-4">
+        <div className="shadow-sm sm:p-5   p-5 w-full rounded-[20px] mb-2  bg-white h-fit">
+          <h1 className="sm:text-[2rem] font-bold">
+            รายชื่อผู้ใช้งาน Line-Login
+          </h1>
+          <ListUser data={user} />
+        </div>
+        <div className="shadow-sm sm:p-5   p-5 w-full rounded-[20px] mb-2  bg-white h-fit">
+          <Profile />
+        </div>
+      </div>
+    </div>
+  );
+}
