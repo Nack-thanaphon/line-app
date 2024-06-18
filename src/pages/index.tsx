4cc62b46492f6e3f2f1dec32ca5c5e9c2c871dee
thanaphon <e21bvz@gmail.com>
2024-06-18 09:12:03 +0700

    update | deploy login verfy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,10 +21,10 @@ export default function Home() {
   const [count, setCount] = useState(0);
   const [totalSend, setTotalSend] = useState(0);
 
-  let LiffID = "2005643824-J4kOP28n";
-  let LiffUrl = "https://liff.line.me/2005643824-J4kOP28n";
-  let LineOa = "https://line.me/R/ti/p/@328uuvhx";
-  let txt =
+  let LiffID = "2005619015-0Bl842BP";
+  let LiffUrl = "https://liff.line.me/2005619015-0Bl842BP";
+  let LineOa = "https://line.me/R/ti/p/@634aahso";
+  let Token =
     "pWqFwpY+tRl3iWvE23+o0cepAVBn/FiIMmsvuSKnJYMUujvdrCboe5/frjph7zSI8uWpCaTv9Yl24UBtSlVJPfsUHhGJlnaNL8qz25UR46BOjE3juEHYsY0G7DF1RQqbnegMGE3RE8cCKb+ajdxgHwdB04t89/1O/w1cDnyilFU=";
 
   const user = async () => {
@@ -47,7 +47,7 @@ export default function Home() {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${txt}`
+            Authorization: `Bearer ${Token}`
           }
         }
       );
